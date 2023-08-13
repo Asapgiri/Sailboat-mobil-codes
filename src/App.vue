26ae645c855be68ed65a8824ca83b14d80dbbcc1
Gergely Korcsak <razzero29@gmail.com>
2023-08-13 13:51:23 +0200

    add bootstrap and such

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <script setup lang="ts">
-import GpsData from './components/GpsData.vue';
-import SensorData from './components/SensorData.vue'
-import WindClock  from './components/WindClock.vue'
+import Meter from './Meter.vue'
+import Dashboard from './Dashboard.vue'
+import Login from './Login.vue'
+
+// routing
+const routes = [
+    { name: 'Dashboard', app: Dashboard, route: '/dashboard' },
+    { name: 'Meter',     app: Meter,     route: '/meter' },
+    { name: 'Login',     app: Login,     route: '/login' },
+]
+
+var CurrentPage = Dashboard
+
+routes.forEach(route =>  {
+    if (route.route == window.location.pathname) {
+        CurrentPage = route.app
+    }
+})
+
+
+
 </script>
 
 <script lang="ts">
@@ -10,15 +28,31 @@ if ("wakeLock" in navigator) {
   var wakeLock = await navigator.wakeLock.request("screen");
   console.log('Wakelog is active..')
 }
+
+
 </script>
 
 <template>
-  
-  <main>
-    <SensorData/>
-    <WindClock/>
-    <GpsData/>
-  </main>
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">Sailboat Multimeter</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample03">
+            <ul class="navbar-nav mr-auto">
+                <li v-for="route in routes" v-class="route.app == CurrentPage ? 'nav-item active' : 'nav-item' ">
+                    <a class="nav-link" v-bind:href="route.route">
+                        {{ route.name }}
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <main class="d-flex justify-content-center">
+        <CurrentPage/>
+    </main>
 </template>
 
 <style scoped>
---
+++ src/Meter.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import GpsData from './components/GpsData.vue';
+import SensorData from './components/SensorData.vue'
+import WindClock  from './components/WindClock.vue'
+</script>
+
+<script lang="ts">
+// create an async function to request a wake lock
+</script>
+
+<template>
+    <SensorData/>
+    <WindClock/>
+    <GpsData/>
+</template>
+
+<style scoped>
+</style>
